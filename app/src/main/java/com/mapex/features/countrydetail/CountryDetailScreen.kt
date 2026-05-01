1ede4e16c531bb5b37c452a04fbcdbb0fe320c84
CamiloCuenca <camilocuenca1810@gmail.com>
2026-05-01 10:50:09 -0500

    Upgrade UI dependencies and refine DetailCard composable

--- app/src/main/java/com/mapex/features/countrydetail/CountryDetailScreen.kt
+++ app/src/main/java/com/mapex/features/countrydetail/CountryDetailScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
@@ -350,7 +351,7 @@ fun CountryDetailContent(
 @Composable
 private fun DetailCard(
     title: String,
-    content: @Composable Column.() -> Unit
+    content: @Composable ColumnScope.() -> Unit
 ) {
     OutlinedCard(
         modifier = Modifier.fillMaxWidth(),
